feb0a95e48a3110297e5767cb07842f95e870aa5
unknown <manhattancb3@gmail.com>
2025-07-23 16:13:47 -0400

    minor updates to comments

--- merge_pdfs.py
+++ merge_pdfs.py
@@ -4,11 +4,19 @@ Description:
     and SLA stipulation documents. It can also be used for Cannabis resolution and stipulations.
 
 
-
-
 Potential updates:
 - create GUI for choosing input folder
-- create separate merge function
+- create separate merge function for general use (not specific to SLA process)
+
+Workflow:
+1) Create PDFs of resolutions and save in "Resolution Document Components" Sharepoint folder
+2) Save all stipulation PDFs in "Resolution Document Components" folder
+3) Delete contents of working folders: "Resolution Document Components" and "SLA Resolutions wSTIPS"
+3) Download the entire "Resolution Document Components" folder (it will be zipped)
+4) Extract zipped/compressed folder contents to "PDF Tools" local folder (where this script is stored)
+5) Run script
+6) Check output files for accuracy
+7) Upload all output files to correct Sharepoint folder
 '''
 
 # Import statements - make sure you pip install if running on new machine
